00db4be3b668e4eacc88c153689f6712ea2d4207
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-06 13:01:36 +0100

    add stubs for operation functions

---
+++ src/rotate.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 12:33:02 by estettle          #+#    #+#             */
+/*   Updated: 2024/11/06 12:58:43 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	ft_ra(t_slab **stack1, t_slab** stack2)
+{
+	
+}
+
+void	ft_rb(t_slab **stack1, t_slab** stack2)
+{
+	
+}
+
+void	ft_rr(t_slab **stack1, t_slab** stack2)
+{
+	ft_ra(stack1, stack2);
+	ft_rb(stack1, stack2);
+}
---
+++ src/swap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 12:33:02 by estettle          #+#    #+#             */
+/*   Updated: 2024/11/06 12:56:22 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	ft_sa(t_slab **stack1, t_slab** stack2)
+{
+	
+}
+
+void	ft_sb(t_slab **stack1, t_slab** stack2)
+{
+	
+}
+
+void	ft_ss(t_slab **stack1, t_slab** stack2)
+{
+	ft_sa(stack1, stack2);
+	ft_sb(stack1, stack2);
+}
+
