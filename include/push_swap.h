bb5a87e57d2ce489309149fed0d13dfee92e9c91
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-06 16:59:21 +0100

    fix header Norm

--- include/push_swap.h
+++ include/push_swap.h
@@ -13,27 +13,27 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-// Linked list element definition
-typedef struct	s_slab
-{
-	int				number;
-	struct s_slab	*next;
-} t_slab;
-
 // Libft
 # include "../libft/libft.h"
 
+// Linked list element definition
+typedef struct s_slab
+{
+	int				number;
+	struct s_slab	*next;
+}	t_slab;
+
 // Operation function prototypes
-void ft_sa(t_slab **stack1, t_slab **stack2);
-void ft_sb(t_slab **stack1, t_slab **stack2);
-void ft_ss(t_slab **stack1, t_slab **stack2);
-void ft_pa(t_slab **stack1, t_slab **stack2);
-void ft_pb(t_slab **stack1, t_slab **stack2);
-void ft_ra(t_slab **stack1, t_slab **stack2);
-void ft_rb(t_slab **stack1, t_slab **stack2);
-void ft_rr(t_slab **stack1, t_slab **stack2);
-void ft_rra(t_slab **stack1, t_slab **stack2);
-void ft_rrb(t_slab **stack1, t_slab **stack2);
-void ft_rrr(t_slab **stack1, t_slab **stack2);
+void	ft_sa(t_slab **stack1, t_slab **stack2);
+void	ft_sb(t_slab **stack1, t_slab **stack2);
+void	ft_ss(t_slab **stack1, t_slab **stack2);
+void	ft_pa(t_slab **stack1, t_slab **stack2);
+void	ft_pb(t_slab **stack1, t_slab **stack2);
+void	ft_ra(t_slab **stack1, t_slab **stack2);
+void	ft_rb(t_slab **stack1, t_slab **stack2);
+void	ft_rr(t_slab **stack1, t_slab **stack2);
+void	ft_rra(t_slab **stack1, t_slab **stack2);
+void	ft_rrb(t_slab **stack1, t_slab **stack2);
+void	ft_rrr(t_slab **stack1, t_slab **stack2);
 
 #endif // PUSH_SWAP_H
