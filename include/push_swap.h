1b58de466d296e77774d6677d99cf74db88e1648
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-06 12:41:00 +0100

    add list element definition

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,12 +6,18 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:46:51 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/04 17:46:51 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/06 12:40:40 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+// Linked list element definition
+typedef struct s_slab
+{
+	int				number;
+	struct s_slab	*next;
+} t_slab;
 
 #endif //PUSH_SWAP_H
