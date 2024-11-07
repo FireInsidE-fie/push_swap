4ef5c16280bb515a14f2bc405975d6f2110768f6
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-07 15:05:11 +0100

    add rotate functions stubs

--- src/rotate.c
+++ src/rotate.c
@@ -14,12 +14,32 @@
 
 void	ft_ra(t_slab **stack1, t_slab **stack2)
 {
-	
+	t_slab	*slab;
+	t_slab	*tmp;
+
+	(void)stack2;
+	slab = *stack1;
+	while (slab->next && (slab->next)->next)
+		slab = slab->next;
+	tmp = slab->next;
+	slab->next = NULL;
+	tmp->next = *stack1;
+	stack1 = &tmp;
 }
 
 void	ft_rb(t_slab **stack1, t_slab **stack2)
 {
-	
+	t_slab	*slab;
+	t_slab	*tmp;
+
+	(void)stack1;
+	slab = *stack2;
+	while (slab->next && (slab->next)->next)
+		slab = slab->next;
+	tmp = slab->next;
+	slab->next = NULL;
+	tmp->next = *stack2;
+	stack2 = &tmp;
 }
 
 void	ft_rr(t_slab **stack1, t_slab **stack2)
