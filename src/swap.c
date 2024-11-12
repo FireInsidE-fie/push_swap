558ed563db157ffa7cf4edb503dbfa187ecdef22
Aisling <aisling.fontaine@protonmail.com>
2024-11-12 17:52:23 +0100

    potentially fix swap functions?

--- src/swap.c
+++ src/swap.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:33:02 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/06 13:13:13 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:51:48 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,32 @@
 
 void	ft_sa(t_slab **stack1, t_slab **stack2)
 {
-	t_slab	*tmp;
+	t_slab	*first;
+	t_slab	*third;
 
 	if (!stack1 || !(*stack1) || !((*stack1)->next))
 		return ;
 	(void)stack2;
-	tmp = ((*stack1)->next)->next;
-	((*stack1)->next)->next = *stack1;
-	(*stack1)->next = tmp;
+	first = *stack1;
+	third = ((*stack1)->next)->next;
+	((*stack1)->next)->next = first;
+	stack1 = &first->next;
+	first->next = third;
 }
 
 void	ft_sb(t_slab **stack1, t_slab **stack2)
 {
+	t_slab	*slab;
 	t_slab	*tmp;
 
 	if (!stack2 || !(*stack2) || !((*stack2)->next))
 		return ;
 	(void)stack1;
-	tmp = ((*stack2)->next)->next;
-	((*stack2)->next)->next = *stack2;
-	(*stack2)->next = tmp;
+	first = *stack2;
+	third = ((*stack2)->next)->next;
+	((*stack2)->next)->next = first;
+	stack2 = &first->next;
+	first->next = third;
 }
 
 void	ft_ss(t_slab **stack1, t_slab **stack2)
