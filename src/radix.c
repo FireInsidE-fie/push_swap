201994aad6dd301261b7d1212e1ded417789dbd1
Aisling <aisling.fontaine@protonmail.com>
2024-11-19 19:16:46 +0100

    fix loop indexes for radix

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 14:59:23 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/19 17:40:10 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/19 19:14:18 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ void	ft_radix(t_slab **stack1, t_slab **stack2)
 	int32_t	max_num;
 	int32_t	max_bits;
 	int32_t	i;
+	int32_t	j;
 
 	size = slab_count(*stack1);
 	max_num = size - 1;
 	max_bits = 0;
-	i = 0;
+	i = -1;
 	while (max_num >> max_bits)
 		max_bits++;
 	while (++i < max_bits)
 	{
-		while (--size > 0)
+		j = 0;
+		while (++j < size)
 		{
 			if ((((*stack1)->index >> i)&1) == 1)
 			{
