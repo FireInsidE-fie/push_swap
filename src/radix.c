919f1be03dd6ebac74a1e4a5579d0fbf579b9828
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-22 12:36:26 +0100

    shorten radix function to Norm

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 14:59:23 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/21 19:19:15 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/22 12:35:54 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	ft_radix(t_slab **stack1, t_slab **stack2)
 	size = slab_count(*stack1);
 	max_bits = get_max_bits(stack1);
 	i = -1;
-	ft_printf("%d\n\n", max_bits);
 	while (++i < max_bits)
 	{
 		j = 0;
@@ -42,21 +41,12 @@ void	ft_radix(t_slab **stack1, t_slab **stack2)
 		{
 			if (!(*stack1))
 				break ;
-			if ((((*stack1)->index >> i) & 1) == 1)
-			{
-				ft_printf("ra\n");
+			if ((((*stack1)->index >> i) & 1) == 1 && ft_printf("ra\n"))
 				ft_ra(stack1, stack2);
-			}
-			else
-			{
-				ft_printf("pb\n");
+			else if (ft_printf("pb\n"))
 				ft_pb(stack1, stack2);
-			}
 		}
-		while (*stack2)
-		{
-			ft_printf("pa\n");
+		while (*stack2 && ft_printf("pa\n"))
 			ft_pa(stack1, stack2);
-		}
 	}
 }
