c51a919333308ab505caa61ad723e64ff6cf9c65
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-10 15:40:59 +0100

    add better algorithm for 3 elements scenarios

--- src/melodies.c
+++ src/melodies.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:03:18 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/10 10:31:17 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/10 15:34:25 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
  */
 void	ft_alis(t_slab **stack1, t_slab **stack2)
 {
-	if (!is_sorted(*stack1))
+	if (*stack1 > (*stack1)->next)
 		ft_sa(stack1, stack2, 0);
 }
 
@@ -26,18 +26,27 @@ void	ft_alis(t_slab **stack1, t_slab **stack2)
  */
 void	ft_roxy(t_slab **stack1, t_slab **stack2)
 {
-	if ((*stack1)->number > (*stack1)->next->number
-		&& (*stack1)->next->number > (*stack1)->next->next->number)
-	{
+	int64_t	first;
+	int64_t	second;
+	int64_t	third;
+
+	first = (*stack1)->number;
+	second = (*stack1)->next->number;
+	third = (*stack1)->next->next->number;
+	if (first > second && second < third && third < first)
 		ft_ra(stack1, stack2, 0);
-		ft_ra(stack1, stack2, 0);
-		return ;
-	}
-	ft_pb(stack1, stack2);
-	ft_alis(stack1, stack2);
-	ft_pa(stack1, stack2);
-	if ((*stack1)->number > (((*stack1)->next)->next)->number)
-		ft_ra(stack1, stack2, 0);
-	else if ((*stack1)->number > ((*stack1)->next)->number)
+	else if (first < second && second > third && third < first)
+		ft_rra(stack1, stack2, 0);
+	else if (first > second && second < third && third > first)
 		ft_sa(stack1, stack2, 0);
+	else if (first > second && second > third && third < first)
+	{
+		ft_sa(stack1, stack2, 0);
+		ft_rra(stack1, stack2, 0);
+	}
+	else if (first < second && second > third && third > first)
+	{
+		ft_sa(stack1, stack2, 0);
+		ft_ra(stack1, stack2, 0);
+	}
 }
