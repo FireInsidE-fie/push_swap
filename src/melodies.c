9d6a7a5d55da35f996bc505c3e43be2a3f232d69
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-10 16:02:31 +0100

    add base for 4 element algo

--- src/melodies.c
+++ src/melodies.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:03:18 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/10 15:34:25 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/10 16:00:24 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,24 @@ void	ft_roxy(t_slab **stack1, t_slab **stack2)
 		ft_sa(stack1, stack2, 0);
 		ft_ra(stack1, stack2, 0);
 	}
+}
+
+void	ft_ash(t_slab **stack1, t_slab **stack2)
+{
+	int64_t	first;
+	int64_t	second;
+	int64_t	third;
+
+	ft_pb(stack1, stack2);
+	ft_roxy(stack1, stack2);
+	first = (*stack1)->number;
+	second = (*stack1)->next->number;
+	third = (*stack1)->next->next->number;
+	if ((*stack2)->number > first && (*stack2)->number > second
+		&& (*stack2)->number > third)
+	{
+		ft_pa(stack1, stack2);
+		ft_ra(stack1, stack2, 0);
+	}
+	// Put the element in stack2 back in stack1 at the right position
 }
