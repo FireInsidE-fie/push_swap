642d079eddf7fd16cf22207e14b6f8f162a40a6d
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-13 14:19:58 +0100

    finish fixing push_back function, melody algo done

--- src/melodies.c
+++ src/melodies.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:03:18 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/11 13:26:56 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:18:49 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,21 +57,24 @@ void	ft_roxy(t_slab **stack1, t_slab **stack2)
  */
 void	push_back(t_slab **stack1, t_slab **stack2)
 {
-	while (*stack2)
+	if ((*stack2)->number > slab_max(stack1))
 	{
-		if ((*stack2)->number > slab_last(*stack1)->number)
-		{
-			ft_pa(stack1, stack2);
+		while (!is_sorted(*stack1))
 			ft_ra(stack1, stack2, FALSE);
-		}
-		else
-		{
-			while ((*stack1)->number < (*stack2)->number)
-				ft_ra(stack1, stack2, FALSE);
-			ft_pa(stack1, stack2);
-			while (!is_sorted(*stack1))
-				ft_ra(stack1, stack2, FALSE);
-		}
+		ft_pa(stack1, stack2);
+		ft_ra(stack1, stack2, FALSE);
+		if (*stack2)
+			push_back(stack1, stack2);
+	}
+	else
+	{
+		while ((*stack1)->number < (*stack2)->number)
+			ft_ra(stack1, stack2, FALSE);
+		ft_pa(stack1, stack2);
+		if (*stack2)
+			push_back(stack1, stack2);
+		while (!is_sorted(*stack1))
+			ft_ra(stack1, stack2, FALSE);
 	}
 }
 
