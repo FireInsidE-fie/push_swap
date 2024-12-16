450502f3d68e215b10c2094d222a7e144872f465
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-16 12:27:19 +0100

    idek

--- src/merge.c
+++ src/merge.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 09:26:22 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/16 10:11:54 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/16 11:32:56 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,22 @@
 
 void	ft_merge(t_slab **stack1, t_slab **stack2)
 {
+	int	i;
 	while (slab_count(*stack1) > slab_count(*stack2))
 		ft_pb(stack1, stack2);
 	// TODO: need a condition in case the number of slabs is uneven
-	while (!is_sorted(*stack1) || !is_sorted(*stack2))
+	i = slab_count(*stack1);
+	while (i-- && (!is_sorted(*stack1) || !is_sorted(*stack2)))
 	{
-		if ((*stack1)->number > (*stack1)->next->number
-			&& (*stack2)->number > (*stack2)->next->number)
+		if ((*stack1)->number > ((*stack1)->next)->number
+			&& (*stack2)->number > ((*stack2)->next)->number)
 			ft_ss(stack1, stack2);
 		else if ((*stack1)->number > (*stack1)->next->number)
 			ft_sa(stack1, stack2, 0);
 		else if ((*stack2)->number > (*stack2)->next->number)
 			ft_sb(stack1, stack2, 0);
 		ft_rr(stack1, stack2);
-		getchar(); // debug
+		// getchar(); // debug
 	}
+	ft_printf("%d\t%d\n", is_sorted(*stack1), is_sorted(*stack2));
 }
