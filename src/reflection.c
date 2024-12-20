195ea9cf33a8586023e028d7856ec3180cf8f966
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-20 08:54:00 +0100

    fix split_push function

--- src/reflection.c
+++ src/reflection.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 09:26:22 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/17 17:51:44 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/19 14:55:53 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,15 @@ static void	split_push(t_slab **stack1, t_slab **stack2)
 	slabs_a = slab_count(*stack1);
 	slice_size = slab_count(*stack1) / 3;
 	index_limit = slabs_a - slice_size;
-	ft_printf("%d\n\n", index_limit);
-	fgetc(stdin);
+	// ft_printf("%d\n\n", index_limit); // debug
 	while (i < slabs_a)
 	{
 		if ((*stack1)->index < index_limit)
 		{
 			ft_pb(stack1, stack2);
-			if ((*stack2)->index > index_limit / 2)
+			if ((*stack2)->index > slice_size)
 				ft_rb(stack1, stack2, FALSE);
+			i++;
 		}
 		else
 			ft_ra(stack1, stack2, FALSE);
@@ -53,5 +53,7 @@ void	ft_reflection(t_slab **stack1, t_slab **stack2)
 	}
 	ft_roxy(stack1, stack2);
 	print_stack(stack1);
+	clear_indexes(*stack2);
+	index_stack(*stack2);
 	print_stack(stack2);
 }
