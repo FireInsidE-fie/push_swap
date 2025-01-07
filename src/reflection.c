75ef5f4350f147b2937bc74d7a9e5164ef7c76c5
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-01-07 17:49:47 +0100

    add stub for push_element function

--- src/reflection.c
+++ src/reflection.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 09:26:22 by estettle          #+#    #+#             */
-/*   Updated: 2025/01/07 16:46:31 by estettle         ###   ########.fr       */
+/*   Updated: 2025/01/07 17:47:07 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,35 @@ static void	split_push(t_slab **stack1, t_slab **stack2)
 	}
 }
 
+/**
+ * @brief Finds the element with the highest "final_position" in stack 2,
+ * brings it to the top of the stack then pushes it onto stack1.
+ */
+static void	push_element(t_slab **stack1, t_slab **stack2, int32_t index)
+{
+	t_slab	*tmp;
+	int		counter;
+
+	counter = 0;
+	tmp = *stack2;
+	while (tmp->final_position != index)
+	{
+		tmp = tmp->next;
+		counter++;
+	}
+	if (counter > slab_count(*stack2))
+	{
+		while ((*stack2)->final_position)
+			ft_rrb(stack1, stack2, FALSE);
+	}
+	else
+	{
+		while ((*stack2)->final_position)
+			ft_rb(stack1, stack2, FALSE);
+	}
+	ft_pa(stack1, stack2);
+}
+
 static void	sort_back(t_slab **stack1, t_slab **stack2)
 {
 	t_slab	*slab;
@@ -58,9 +87,10 @@ static void	sort_back(t_slab **stack1, t_slab **stack2)
 		tmp = tmp->next;
 	}
 	index = slab->final_position;
-	(void)index;
 	// then pretty much just push each element index by index, decreasing to end
 	// with a sorted stack1
+	while (index && *stack2)
+		push_element(stack1, stack2, index--);
 }
 
 void	ft_reflection(t_slab **stack1, t_slab **stack2)
