51e1fbc41b138b79d5accc7dae0f0d394eadc9eb
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-01-10 15:13:30 +0100

    did some theory crafting

--- src/reflection.c
+++ src/reflection.c
@@ -16,6 +16,20 @@
 
 #define SLICE_COUNT 2
 
+/* Time for some theory crafting
+ * Imagine if on stack1, I had a tiny stack of unordered elements that I can just
+ * get from stack2 and store at the very end of the first stack for future use.
+ * It could be called something like cache and I could just fetch values from it.
+ * For example, my stack 1 is currently 9 8 7, with 1 at the top. The next number
+ * in stack2 is 5, so that won't do. I'll push it to stack1 but rotate it into
+ * the "cache" for future use. Great news, next element in stack 2 is 6, so I can
+ * directly push it. Now, where is 5 located? Oh right, in the cache! I just have
+ * to reverse rotate to get it, and focus on getting the rest of the stack2 in the
+ * same manner.
+ * The idea would be to start with only a 1 number sized cache, and then maybe
+ * try with bigger caches to see if the optimization would be substantial.
+*/
+
 static void	split_push(t_slab **stack1, t_slab **stack2)
 {
 	int	i;
