de3e6ae16914d5de8f0574285ad538816797c2c2
Aisling <aisling.fontaine@protonmail.com>
2025-02-04 22:07:46 +0100

    finish test.sh

--- test.sh
+++ test.sh
@@ -26,3 +26,60 @@ read -r meh
 
 valgrind --leak-check=full ./push_swap "42 -42 66"
 valgrind --leak-check=full ./push_swap "-42 66 42"
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 3` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+
+echo "[!] - Beginning checks for 5 elements..."
+
+read -r meh
+
+valgrind --leak-check=full ./push_swap "42 -42 66 877 233"
+valgrind --leak-check=full ./push_swap "-42 66 42 899 123"
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 5` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+
+echo "[!] - Beginning checks for 100 elements..."
+
+read -r meh
+
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 100` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+
+echo "[!] - Beginning checks for 500 elements..."
+
+read -r meh
+
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
+ARG=`shuf -i 0-100000 -n 500` && valgrind --leak-check=full ./push_swap $ARG | ./checkers/checker_linux $ARG
